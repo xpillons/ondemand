c7ed62ba9fa3486d78ec082936174c055e6aa45e
Mario Squeo <msqueo@osc.edu>
2021-05-21 13:08:16 -0400

    Add fetchRemote and updateWidgetDOM

--- apps/dashboard/app/javascript/controllers/poller_controller.js
+++ apps/dashboard/app/javascript/controllers/poller_controller.js
@@ -4,6 +4,8 @@
 
 import { Controller } from 'stimulus'
 
+import axios from 'axios'
+
 export default class extends Controller {
   static targets = [
     'output'
@@ -14,8 +16,39 @@ export default class extends Controller {
     url: String
   }
 
-  initialize() {
+  http = axios.create({
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  })
 
+  async fetchRemote() {
+    const { data } = await axios.get(this.urlValue)
+
+    if (!data) {
+      console.log('ERROR')
+    }
+
+    return data
+  }
+
+  updateWidgetDOM(data) {
+    if (typeof data == 'string') {
+      this.outputTarget.innerHTML = data
+    } else {
+      if (typeof data == Object) {
+        console.info('OBJ')
+      }
+      this.outputTarget.innerHTML = 'Failed to update DOM'
+    }
+  }
+
+  initialize() {
+    // Setup request
+    this.fetchRemote()
+      .then(data => {
+        this.updateWidgetDOM(data)
+      })
   }
 
   connect() {
